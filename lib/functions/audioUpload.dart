632447e5850ded875036b8f55314b9ea2b870059
Kapetanios99 <brendanmahadeo@gmail.com>
2024-02-03 16:52:20 -0500

    did some audio stuff

--- lib/functions/audioUpload.dart
+++ lib/functions/audioUpload.dart
@@ -1,11 +1,32 @@
-import 'package:file_picker/file_picker.dart';
+import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 Future<PlatformFile> uploadAudio() async {
   FilePickerResult? result = await FilePicker.platform.pickFiles();
-
+  
   if (result != null) {
-    PlatformFile file = result.files.first;
+      PlatformFile file = result.files.first;
+      // var request = http.MultipartRequest("POST", Uri.parse('http://10.216.216.153:5000/upload'));
+
+      // request.files.add(http.MultipartFile.fromBytes(
+      //   'audio',
+      //   file.bytes!,
+      //   filename: file.name,
+      // ));
+
+      // request.headers['Content-Type'] = 'multipart/form-data';
+
+      // var response = await request.send();
+
+      // if (response.statusCode == 200) {
+      //   print("File uploaded successfully");
+      // } else {
+      //   print("Failed to upload file. Status code: ${response.statusCode}");
+      // }
+
 
     return file;
   } else {
--- lib/widgets/futureFile.dart
+++ lib/widgets/futureFile.dart
@@ -1,6 +1,8 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:call_safe/functions/audioUpload.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class FutureFile extends StatefulWidget {
   const FutureFile({
@@ -17,9 +19,14 @@ class _FutureFileState extends State<FutureFile> {
   Widget build(BuildContext context) {
     return FutureBuilder<PlatformFile> (
       future: uploadAudio(),
+      
+
 
       builder: (BuildContext context, AsyncSnapshot<PlatformFile> snapshot) {
         if (snapshot.hasData) {
+
+         
+          
           return Text(snapshot.data!.name);
         }
         else {
